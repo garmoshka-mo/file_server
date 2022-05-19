c43b3082630837daf045292ca39ff0f964b304c1
Daniel <d@coav.ru>
2022-05-19 23:41:26 +0300

    resize image on-the-fly

--- image_server.js
+++ image_server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const timeout = require('connect-timeout')
 require('dotenv').config()
 var execFile = require("child_process").execFile
+const sharp = require('sharp');
 
 const app = express()
 const port = 8080
@@ -11,7 +12,13 @@ app.use(express.static('image_server'))
 app.use(timeout(20 * 60 * 1000)) // 20 minutes
 
 app.get('/thumbnail/:file', (req, res) => {
-    res.sendFile(`image_server/${req.params.file}`, {root: __dirname});
+
+  sharp(`image_server/${req.params.file}`)
+    .resize(150)
+    .jpeg({ mozjpeg: true })
+    .toBuffer()
+    .then(data => res.type('jpeg').send(data))
+
   }
 )
 
